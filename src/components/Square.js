688babb316a784ac659db0a7b04f7feeb9a31b9a
ZackDinerstein <zackdhome@gmail.com>
2018-11-18 14:13:23 -0500

    Updated component file structure

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        
+      </button>
+    );
+  }
+}
